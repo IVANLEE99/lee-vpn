9f1a5e2e30dfeb4b330e6805bfeb379a1fd90285
871246995 <871246995@qq.com>
2018-01-23 12:54:46 +0800

    port

--- local.js
+++ local.js
@@ -9,15 +9,15 @@ local
     .option('-s --server-address <address>', 'server address')
     .option('-p --server-port <port>', 'server port, default: 8388')
     .option('-b --local-address <address>', 'local binding address, default: 127.0.0.1')
-    .option('-l --local-port <port>', 'local port, default: 1080')
+    .option('-l --local-port <port>', 'local port, default: 5000')
     .option('--log-level <level>', 'log level(debug|info|warn|error|fatal)', /^(debug|info|warn|error|fatal)$/i, 'info')
     .option('--log-file <file>', 'log file')
     .parse(process.argv);
 
 var relay = new TCPRelay({
     localAddress: process.env['LOCALADDRESS'] || local.localAddress || '127.0.0.1',
-    // localPort: process.env['LOCALPORT'] || local.localPort || 1080,
-    localPort: process.env.PORT || local.localPort || 5000,
+    localPort: process.env['LOCALPORT'] || local.localPort || 5000,
+    // localPort: process.env.PORT || local.localPort || 5000,
     serverAddress: process.env['SERVERADDRESS'] ||local.serverAddress || 'leevpn.herokuapp.com' ||'127.0.0.1',
     serverPort: process.env['SERVERPORT'] ||local.serverPort || 8388,
     password: process.env['PASSWORD'] || local.password || 'woshilixiang' || 'shadowsocks-over-websocket',
